8723de6f5e4d14e76b194d2e9aaafac26344a93f
Konstantin <konstantin@yva.ai>
2019-04-01 20:17:59 +0300

    Refactor string tests

--- Lession2/test_string.py
+++ Lession2/test_string.py
@@ -1,14 +1,14 @@
 """Here are the tests with string"""
 
 
-def test_string_length_and_space(string_prepare, random_text_length):
+def test_string_length_and_space(string_prepare, random_number):
     """Test checks the length of string, depending on random text length,
     and that string does not start with a space"""
-    assert len(string_prepare) == 22 + random_text_length
+    assert len(string_prepare) == 22 + random_number
     assert not string_prepare.startswith(" ")
 
 
-def test_all_parts(string_prepare, random_text_length):
+def test_all_parts(string_prepare, random_number):
     """This test checks if the string starts with known start and end parts,
     and whether random part consists of letters in upper case"""
 
@@ -16,14 +16,14 @@ def test_all_parts(string_prepare, random_text_length):
     end_of_the_string = " here"
 
     assert string_prepare.startswith(start_of_the_string)
-    string_prepare = string_prepare.lstrip(start_of_the_string)
+    string_prepare = string_prepare.replace(start_of_the_string, "")
 
     assert string_prepare.endswith(end_of_the_string)
-    string_prepare = string_prepare.rstrip(end_of_the_string)
+    string_prepare = string_prepare.replace(end_of_the_string, "")
 
     assert string_prepare.isupper()
     assert string_prepare.isalpha()
 
-    assert len(string_prepare) == random_text_length
+    assert len(string_prepare) == random_number
 
 
